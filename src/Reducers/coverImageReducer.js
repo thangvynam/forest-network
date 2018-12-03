52fc6db205b349baf3634bc4b54985b8ddd820e2
BARCELONA\user <hoangphuct97@gmail.com>
2018-12-03 22:06:24 +0700

    Add follower dialog, create new followReducer

--- src/Reducers/coverImageReducer.js
+++ src/Reducers/coverImageReducer.js
@@ -1,9 +1,8 @@
-import {OPEN_DIALOG_CONFIG, OPEN_DIALOG_FOLLOWING} from '../Constant/actionTypes'
+import {OPEN_DIALOG_CONFIG} from '../Constant/actionTypes'
 import {EDIT_PROFILE} from '../Constant/actionTypes'
 
 const coverImageInitialState = {
     open :false ,
-    openFollowing: false,
     name : "Unknown",
     location : "Unkown"
 }
@@ -11,8 +10,6 @@ const coverImageReducer = (state = coverImageInitialState, action) => {
     switch (action.type) {
         case OPEN_DIALOG_CONFIG:
             return {...state,open:action.open}
-        case OPEN_DIALOG_FOLLOWING:
-            return {...state, openFollowing: action.openFollowing}
         case EDIT_PROFILE:
             return {...state,open:action.open,name:action.name,location:action.location}    
         default:
--- src/Store/Store.js
+++ src/Store/Store.js
@@ -1,9 +1,11 @@
 import coverImageReducer from "../Reducers/coverImageReducer"
 import tweetReducer from "../Reducers/tweetReducer"
+import followReducer from "../Reducers/followReducer"
 var redux = require("redux");
 const allReducers = redux.combineReducers({
     coverImageReducer:coverImageReducer,
-    tweetReducer:tweetReducer 
+    tweetReducer:tweetReducer,
+    followReducer: followReducer
 })
 var store1 = redux.createStore(allReducers);
 export default store1;
